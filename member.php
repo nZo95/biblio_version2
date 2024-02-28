3ba785b961c2669681c93753680f0e2cd9ae345a
Sofiane Ouadah <sofiane.ouadah.pro@gmail.com>
2024-02-28 18:39:09 +0100

    Début
    Début de l'espace membre

--- member.php
+++ member.php
@@ -1,15 +1,115 @@
 <!DOCTYPE html>
-
 <html lang="fr">
-  <?php require('header.php'); ?>
-  <body>
-        <nav>
-            <ul> 
-                <li>WISHLIST</li>
-                <li>RESERVATION</li>
-                <li>PARAMETRE</li>
-            </ul>
-        </nav>
-  </body>
-  <?php require('footer.php'); ?>
+<?php require('header.php'); ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espace Membre ASF</title>
+    <link rel="stylesheet" href="member_area/styles/member.css">
+</head>
+<body>
+  
+    <div class="container">
+
+          <!-- ============ Barre de navigation ============ -->
+
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="library-outline"></ion-icon>
+                        </span>
+                        <span class="title">ASF</span>
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="#">
+
+                        <span class="icon">
+
+                          <ion-icon name="home-outline"></ion-icon>
+
+                        </span>
+
+                        <span class="title">Accueil</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="star-outline"></ion-icon>
+                        </span>
+                        <span class="title">Wish-List</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                          <ion-icon name="bookmark-outline"></ion-icon>
+                        </span>
+                        <span class="title">Réservation</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                          <ion-icon name="settings-outline"></ion-icon>
+                        </span>
+                        <span class="title">Paramètre</span>
+                    </a>
+                </li>
+        </div>
+
+               <!-- ============ Menu ============ -->
+
+        <div class="main">
+
+            <div class="topbar">
+
+                <div class="toggle">
+
+                    <ion-icon name="menu-outline"></ion-icon>
+
+                </div>
+
+            </div>
+
+            <!-- ============ Résa en cours ============ -->
+
+            <div class="details">
+                <div class="recentOrders">
+                    <div class="cardHeader">
+
+                        <div>
+                          <h1>Bienvenue dans l'espace Membre !</h1>
+
+                          <p>ffdfdffddffd</p>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div> <!-- fin du main -->
+
+    </div> <!-- fin du container -->
+
+     <!-- ============ Script ============ -->
+
+     <script src="admin_interface_BBV2/assets/js/main.js"></script>
+
+
+     <!-- ============ Pour les îcones ============ -->
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
 </html>
