b758f7f5b5c2bd1714761ca8ed7f062924704fbf
Kyllian_vv <137498455+Kelvbb@users.noreply.github.com>
2024-02-28 08:13:27 -0800

    SQL | script

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() { });
+
+let btnFind = document.getElementById('btnFind');
+
+btnFind.onclick = find;
+
+function find() {
+    let divFind = document.getElementById('findDiv');
+
+    divFind.style.display = 'block';
+}
