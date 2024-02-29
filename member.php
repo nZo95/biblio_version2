7a3eb91891c67ecaa710b77ce54d58b7a1173b74
Sofiane Ouadah <sofiane.ouadah.pro@gmail.com>
2024-02-29 15:45:50 +0100

    New_area_member
    Recommencement de l'espace membre

--- member.php
+++ member.php
@@ -1,165 +1,50 @@
 <!DOCTYPE html>
 <html lang="fr">
-
+<?php require 'header.php'?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Espace Membre ASF</title>
+    <title>Espace Membre - ASF</title>
     <link rel="stylesheet" href="member_area/styles/member.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0">
+    
 </head>
 <body>
-  
-    <div class="container">
+    <section>
 
-          <!-- ============ Barre de navigation ============ -->
-
-        <div class="navigation">
-
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class="icon">
-
-                            <ion-icon name="library-outline"></ion-icon>
-
-                        </span>
-
-                        <span class="title">ASF</span>
-
-                    </a>
-                </li>
-
-
-                <li>
-                    <a href="member.php">
-
-                        <span class="icon">
-
-                          <ion-icon name="home-outline"></ion-icon>
-
-                        </span>
-
-                        <span class="title">Accueil</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="member_area/wish.php">
-
-                        <span class="icon">
-
-                            <ion-icon name="star-outline"></ion-icon>
-
-                        </span>
-
-                        <span class="title">Favoris </span>
-
-                    </a>
-                </li>
-
-                <li>
-                    <a href="member_area/setting.php">
-
-                        <span class="icon">
-
-                          <ion-icon name="settings-outline"></ion-icon>
-
-                        </span>
-                        
-                        <span class="title">Paramètre</span>
-
-                    </a>
-                </li>
-            </ul>
+        <div class="space">
+            <h1>Bienvenue sur votre espace membre</h1>
+            <img class="sizeimg" src="member_area/img/compte.png" alt="">
+            
         </div>
 
-               <!-- ============ Menu ============ -->
-
-        <div class="main">
-
-            <div class="topbar">
-
-                <div class="toggle">
-
-                    <ion-icon name="menu-outline"></ion-icon>
-
+        <div class="space">
+            <h1>Favoris</h1>
+            <div class="container">
+                <div class="slider-wrapper">
+                    <button id="prev-slide" class="slide-button material-symbols-rounded">chevron_left</button>
+                    <div class="image-list">
+                        <img src="images/9780090898305.jpg" alt="" class="image-item">
+                        <img src="images/9780151001637.jpg" alt="" class="image-item">
+                        <img src="images/9780307792365.jpg" alt="" class="image-item">
+                        <img src="images/9780613135818.jpg" alt="" class="image-item">
+                        <img src="images/9782253087830.jpg" alt="" class="image-item">
+                    </div>
+                    <button id="next-slide" class="slide-button material-symbols-rounded">chevron_right</button>
                 </div>
-
             </div>
-
-            <div class="details">
-
-                <div class="recentOrders">
-
-
-                    <div class="cardHeader">
-
-                    <h1 class="mod">Bienvenue</h1>
-
-                    </div>
-
-                    <div>
-
-                        <p>dsgfsfs</p>
-
-                    </div>
-
+            <div class="slider-scrollbar">
+                <div class="scrollbar-track">
+                    <div class="scrollbar-thumb"></div>
                 </div>
-
             </div>
+        </div>
 
-            <div class="details">
+        <div class="space">
+            <h1>Paramètre</h1>
+        </div>
 
-                <div class="recentOrders">
-
-                    <div class="cardHeader">
-
-                        <h1 class="mod">Favoris</h1>
-
-                    </div>
-
-                    <div>
-
-                        <p>dsgfsfs</p>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="details">
-
-                <div class="recentOrders">
-
-                    <div class="cardHeader">
-
-                        <h1 class="mod">Paramètre</h1>
-
-                    </div>
-
-                    <div>
-
-                        <p>dsgfsfs</p>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        </div> <!-- fin du main -->
-
-    </div> <!-- fin du container -->
-
-     <!-- ============ Script ============ -->
-
-     <script src="admin_interface_BBV2/assets/js/main.js"></script>
-
-
-     <!-- ============ Pour les îcones ============ -->
-
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </section>
 </body>
+<?php require 'footer.php'?>
 </html>
