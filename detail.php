6c5126cd4ee79ee8e7be5669ac5717a7964c3ae5
Mojito <enzox9191@gmail.com>
2024-03-06 12:48:07 +0100

    detail

---
+++ detail.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+  <?php require('header.php'); ?>
+  <head>
+    <link type="text/css" rel="stylesheet" href="styles/detail.css">
+  </head>
+  <body>
+    <main>
+      <div class="space"></div>
+      <h2>Axiomatique de Greg EGAN</h2>
+      <div class="space"><hr></div>
+      <div class="container-book">
+        <div class="book">
+          <div class="image-item">
+            <img src="images/9782253087830.jpg" alt="">
+          </div>
+          <div class="text-item">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="space"></div>
+    </main>
+  </body>
+  <?php require('footer.php'); ?>
+</html>
