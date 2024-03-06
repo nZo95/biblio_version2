8081bc6cc57b1935cabd0828559d6ae1f7d40ca5
Mojito <enzox9191@gmail.com>
2024-03-06 14:20:43 +0100

    Update detail

--- detail.php
+++ detail.php
@@ -16,7 +16,11 @@
             <img src="images/9782253087830.jpg" alt="">
           </div>
           <div class="text-item">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <p class="center">Livre de Science-fiction</p>
+            <p class="center">Edition Millenium, publié en 1995</p>
+            <p class="center">Langue Originale : Anglais</p>
+            <h3>Résumé : </h3>
+            <p class="resume">Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
           </div>
         </div>
       </div>
