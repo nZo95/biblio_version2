6b354aa892b6c1f1ee15b3c57a201d2f45897741
EvilKroma <deneuchateldorian205@gmail.com>
2024-03-06 11:09:44 +0100

    Réorganisation

---
+++ index_admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interface Administrateur ASF</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- ============ Barre de navigation ============ -->
+    
+     <!-- ============ Pour les îcones ============ -->
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
