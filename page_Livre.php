5ab4f331f97eae07383602cb80b08c88730ab920
Kyllian_vv <137498455+Kelvbb@users.noreply.github.com>
2024-03-06 05:21:03 -0800

    Page Livre

---
+++ page_Livre.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php require('header.php'); ?>
+<head>
+    <link type="text/css" rel="stylesheet" href="styles/home.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0">
+  </head>
+  <body>
+    <main>
+      <div class="space"></div>
+      <div class="container-slider-wrapper">
+        <div class="slider-wrapper">
+          <button id="prev-slide" class="slide-button material-symbols-rounded">chevron_left</button>
+          <div class="image-list">
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+          </div>
+          <button id="next-slide" class="slide-button material-symbols-rounded">chevron_right</button>
+        </div>
+      </div>
+      <div class="slider-scrollbar">
+        <div class="scrollbar-track">
+          <div class="scrollbar-thumb"></div>
+        </div>
+      </div>
+      <div class="space"><hr></div>
+      <section id="section_genre">
+        <section id="searchBar">
+            <h2>Entrez dans le monde merveilleux de la lecture, une aventure rempli de mystère...</h2>
+            <input type="text">
+        </section>
+        <H2>Découvrir par <em>Genre</em></H2>
+        <div id="fantastique" class="genre">
+            <h3>Fantastique</h3>
+            <div class="space"></div>
+      <div class="container-slider-wrapper">
+        <div class="slider-wrapper">
+          <button id="prev-slide" class="slide-button material-symbols-rounded">chevron_left</button>
+          <div class="image-list">
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+          </div>
+          <button id="next-slide" class="slide-button material-symbols-rounded">chevron_right</button>
+        </div>
+      </div>
+      <div class="slider-scrollbar">
+        <div class="scrollbar-track">
+          <div class="scrollbar-thumb"></div>
+        </div>
+      </div>
+      <div class="space"><hr></div>
+        </div>
+        <div id="Thriller" class="genre">
+            <h3>Thriller</h3>
+            <div class="space"></div>
+      <div class="container-slider-wrapper">
+        <div class="slider-wrapper">
+          <button id="prev-slide" class="slide-button material-symbols-rounded">chevron_left</button>
+          <div class="image-list">
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+          </div>
+          <button id="next-slide" class="slide-button material-symbols-rounded">chevron_right</button>
+        </div>
+      </div>
+      <div class="slider-scrollbar">
+        <div class="scrollbar-track">
+          <div class="scrollbar-thumb"></div>
+        </div>
+      </div>
+      <div class="space"><hr></div>
+        </div>
+        <div id="Horreur" class="genre">
+            <h3>Horreur</h3>
+            <div class="space"></div>
+      <div class="container-slider-wrapper">
+        <div class="slider-wrapper">
+          <button id="prev-slide" class="slide-button material-symbols-rounded">chevron_left</button>
+          <div class="image-list">
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+          </div>
+          <button id="next-slide" class="slide-button material-symbols-rounded">chevron_right</button>
+        </div>
+      </div>
+      <div class="slider-scrollbar">
+        <div class="scrollbar-track">
+          <div class="scrollbar-thumb"></div>
+        </div>
+      </div>
+      <div class="space"><hr></div>
+        </div>
+        <div id="Surnaturel" class="genre">
+            <h3>Surnaturel</h3>
+            <div class="space"></div>
+      <div class="container-slider-wrapper">
+        <div class="slider-wrapper">
+          <button id="prev-slide" class="slide-button material-symbols-rounded">chevron_left</button>
+          <div class="image-list">
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+            <div class="image-item"><img src="images/9782253087830.jpg" alt=""></div>
+          </div>
+          <button id="next-slide" class="slide-button material-symbols-rounded">chevron_right</button>
+        </div>
+      </div>
+      <div class="slider-scrollbar">
+        <div class="scrollbar-track">
+          <div class="scrollbar-thumb"></div>
+        </div>
+      </section>
+      <?php require('footer.php'); ?>
+</body>
+</html>
