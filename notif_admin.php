33591b8dcdf4b38ac97c86d72e23e591dba496e2
EvilKroma <deneuchateldorian205@gmail.com>
2024-03-07 12:36:03 +0100

    Page d'activation de comptes

--- notif_admin.php
+++ notif_admin.php
@@ -11,7 +11,50 @@
 <?php
 require "header.php";
 ?>
+    <h1>Demandes d'activations de compte</h1>
 
+    <div class="account_container">
+        <div class="request_card">
+            <h2>Nom Prénom</h2>
+            <p>Identifiant : XXXX</p>
+            <p>Date de demande : XX/XX/XXXX</p>
+            <button>Autoriser</button>
+            <button>Refuser</button>
+        </div>
+
+        <div class="request_card">
+            <h2>Nom Prénom</h2>
+            <p>Identifiant : XXXX</p>
+            <p>Date de demande : XX/XX/XXXX</p>
+            <button>Autoriser</button>
+            <button>Refuser</button>
+        </div>
+
+        <div class="request_card">
+            <h2>Nom Prénom</h2>
+            <p>Identifiant : XXXX</p>
+            <p>Date de demande : XX/XX/XXXX</p>
+            <button>Autoriser</button>
+            <button>Refuser</button>
+        </div>
+
+        <div class="request_card">
+            <h2>Nom Prénom</h2>
+            <p>Identifiant : XXXX</p>
+            <p>Date de demande : XX/XX/XXXX</p>
+            <button>Autoriser</button>
+            <button>Refuser</button>
+        </div>
+
+        <div class="request_card">
+            <h2>Nom Prénom</h2>
+            <p>Identifiant : XXXX</p>
+            <p>Date de demande : XX/XX/XXXX</p>
+            <button>Autoriser</button>
+            <button>Refuser</button>
+        </div>
+    </div>
+    
     
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
