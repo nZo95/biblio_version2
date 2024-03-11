1a0913b1d6fda4f97fbcfc092e3dfed1e8469509
EvilKroma <deneuchateldorian205@gmail.com>
2024-03-11 13:16:06 +0100

    Refait json

--- json_admin.php
+++ json_admin.php
@@ -11,8 +11,14 @@
 <?php
 require "header.php";
 ?>
-    <div class="space"></div>
-    <h1 class="h1_json">Ajoutez un JSON</h1>
-    <div class="space"><hr></div>
+
+    <div class="json_container">
+        <p>Importer le JSON :</p>
+        <a href=""><ion-icon name="cloud-download-outline"></ion-icon></a>
+    </div>
+    
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="scripts/admin.js"></script>
 </body>
 </html>
