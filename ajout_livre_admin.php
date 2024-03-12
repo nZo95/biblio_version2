854b936e2729617942c99f8986ec1bfbb93dd9e1
EvilKroma <deneuchateldorian205@gmail.com>
2024-03-12 11:24:49 +0100

    Modif ordre input ajout livre

--- ajout_livre_admin.php
+++ ajout_livre_admin.php
@@ -13,27 +13,36 @@ require "header.php";
 ?>
 
     <div class="book-card">
+
         <div class="left-panel">
-            <input type="text" placeholder="Titre du livre" class="input"/>
-            <input type="text" placeholder="Auteur" class="input"/>
-            <input type="text" placeholder="Editeur" class="input"/>
+
             <input type="text" placeholder="ISBN" class="input"/>
-            <input type="text" placeholder="Genre" class="input"/>
-            <input type="text" placeholder="Langue" class="input"/>
+
+            <input type="text" placeholder="Titre du livre" class="input"/>
+
             <input type="date" placeholder="Date de publication" class="input"/>
+
+            <input type="text" placeholder="Genre" class="input"/>
+
+            <input type="text" placeholder="Editeur" class="input"/>
+
+            <input type="text" placeholder="Auteur" class="input"/>
+
+            <input type="text" placeholder="Langue" class="input"/>
+
             <textarea placeholder="Description du livre" rows="4" class="input"></textarea>
             <button class="send-button">Envoyer</button>
         </div>
+
         <div class="right-panel">
-    <input type="file" id="imageInput" style="display: none;" accept="image/*" />
-    <div class="add-image" onclick="document.getElementById('imageInput').click();">
-        <ion-icon name="add-circle-outline"></ion-icon>
-        <p>Ajouter une image de couverture</p>
-    </div>
-    <button id="removeImageButton" style="display: none;">Retirer l'image</button>
-    <img id="coverImage" style="max-width: 100%; display: none;"/>
-</div>
-        
+            <input type="file" id="imageInput" style="display: none;" accept="image/*" />
+            <div class="add-image" onclick="document.getElementById('imageInput').click();">
+                <ion-icon name="add-circle-outline"></ion-icon>
+                <p>Ajouter une image de couverture</p>
+            </div>
+            <button id="removeImageButton" style="display: none;">Retirer l'image</button>
+            <img id="coverImage" style="max-width: 100%; display: none;"/>
+        </div>
     </div>
     
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
