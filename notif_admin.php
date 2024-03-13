3dde41964e8b11644d70d1ca7999449c32618092
EvilKroma <deneuchateldorian205@gmail.com>
2024-03-13 17:29:36 +0100

    Update notif_admin.php

--- notif_admin.php
+++ notif_admin.php
@@ -14,13 +14,12 @@ require "header.php";
 if(isset($_GET["id"]) && isset($_GET["action"]))
 {
     $sql_refuse = "DELETE FROM inscription WHERE id = '".$_GET['id']."';";
-    
+
     if($_GET["action"] == "accept"){
         $sql_get_mdp = mysqli_fetch_array(mysqli_query($link, 'SELECT mdp FROM inscription WHERE id = "' . $_GET['id'] . '";'));
         $sql_accept = "INSERT INTO compte (id, admin, nom, prenom, mdp) VALUES ('".$_GET['id']."', 0, ' ', ' ', '" . $sql_get_mdp['mdp'] . "');";        
         mysqli_query($link, $sql_accept);
     }
-
     mysqli_query($link, $sql_refuse);
 } 
 
