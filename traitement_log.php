f3fc6ce3213446cc7d3afc772fff3442f6b87025
Sofiane Ouadah <sofiane.ouadah.pro@gmail.com>
2024-03-14 12:43:48 +0100

    Update inscription
    Problem  inscriptioon solved

--- traitement_log.php
+++ traitement_log.php
@@ -2,24 +2,25 @@
     $bdd = new PDO('mysql:host=localhost;dbname=bibliov2;charset=utf8;', 'root', '');
     if(isset($_POST['envoi'])){
         if(!empty($_POST['id']) AND !empty($_POST['newPassword'])){
-            $pseudo = htmlspecialchars($_POST['id']);
+            $codeetu = htmlspecialchars($_POST['id']);
             $mdp = sha1($_POST['newPassword']);
-            $insertUser = $bdd->prepare('INSERT INTO inscription(id, mdp)VALUES(?, ?)');
-            $insertUser->execute(array($pseudo, $mdp));
+            try {
+                $insertUser = $bdd->prepare('INSERT INTO inscription(id, mdp)VALUES(?, ?)');
+                $insertUser->execute(array($codeetu, $mdp));
  
-            $recupUser = $bdd->prepare('SELECT * FROM inscription WHERE id = ? AND mdp = ?');
-            $recupUser->execute(array($pseudo,$mdp ));
-            if($recupUser->rowCount() > 0){
+                $recupUser = $bdd->prepare('SELECT * FROM inscription WHERE id = ? AND mdp = ?');
+                $recupUser->execute(array($codeetu,$mdp ));
+                if($recupUser->rowCount() > 0){
+                   
+                    $_SESSION['idetu'] = $codeetu;
+                    $_SESSION['mdp'] = $mdp;
+                    $_SESSION['id'] = $recupUser->fetch()['id'];
                
-                $_SESSION['id'] = $pseudo;
-                $_SESSION['newPassword'] = $mdp;
-                $_SESSION['id'] = $recupUser->fetch()['id'];
-           
-            }
+                }
+            } catch(Exception $e) { }
             header('Location: inscription_completed.php');
         }else{
             echo "Veuillez compléter tout les champs";
         }
     }
-
-
+?>
