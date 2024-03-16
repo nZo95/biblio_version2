709acedbec02884ebcd5e9a7b2589bbaf2c64d7f
EvilKroma <deneuchateldorian205@gmail.com>
2024-03-16 15:45:02 +0100

    update

--- ajouterEditeur.php
+++ ajouterEditeur.php
@@ -11,14 +11,10 @@
     <?php
      
         $libelleEditeur = $_POST['editeur'];
-
-        // Requête pour compter le nombre d'éditeurs existants
         $sqlCount = "SELECT MAX(id) as maxId FROM editeur";
         $result = $link->query($sqlCount);
         $row = $result->fetch_assoc();
         $newId = $row['maxId'] + 1;
-
-        // Requête SQL pour insérer le nouvel éditeur avec l'ID calculé
         $sqlInsert = "INSERT INTO editeur (id, libelle) VALUES ($newId, '$libelleEditeur')";
 
         if ($link->query($sqlInsert) === TRUE) {
